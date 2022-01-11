3588301bfb21a05b45881e14a4bbd22ec9a819d7
sidigui <97124960+sidigui@users.noreply.github.com>
2022-01-11 18:35:06 +0200

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+// TYPED
+
+var typed = new Typed('.typed', {
+    strings: ["Actuellement étudiante en master 1 Droit Public parcours Droit et Économie publics des Affaires, je suis aussi travailleuse sociale entant qu'Assistante de Vie"],
+    typeSpeed: 20,
+   
+  });
+
+
+  // AOS
+
+  Aos.init();
